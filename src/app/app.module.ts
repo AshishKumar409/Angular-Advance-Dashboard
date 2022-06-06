b0347190a7541bef2d8788cec5e94e162c792a4f
AshishKumar409 <ashishkumarnath9@gmail.com>
2022-06-06 19:19:35 +0530

    day 1-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    BookmarksComponent
   ],
   imports: [
     BrowserModule,
